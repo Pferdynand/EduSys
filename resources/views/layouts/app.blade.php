374d26aa364e3df110a33aa1ed65006e30a6f9b1
Pferdynand <pferd.fs@gmail.com>
2023-06-19 18:46:02 -0400

    Modificar Dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,8 +20,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/svg/LogoFuego.svg" style="height: 25px; border-right: 1px solid #333;" class="pr-3 pt-2"></div>
+                    <div class="pl-3 pt-2">Cristo Salvador</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
